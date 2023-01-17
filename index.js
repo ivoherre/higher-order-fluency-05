3d46136060612c0d5be028d3f7fe197ebf9b4ad9
ivon-herrera <i.herrera98@hotmail.com>
2023-01-17 16:02:10 +0000

    blah

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const alumni = [
+{name:'Jarrit', job:'TPT',language:'JavaScript', age:23}, 
+{name:'Stephanie', job:'JPMorgan',language:'JavaScript', age:24}, 
+{name:'Devonte', job:'WW',language:'JavaScript', age:23}, 
+{name:'Enmanuel', job:'Asana',language:'JavaScript', age:23},
+{name:'Shemar', job:'SquareSpace',language:'JavaScript', age:23},
+{name:'Cielo', job:'NYT',language:'JavaScript', age:22},
+{name:'Carmen', job:'Marcy Lab School',language:'JavaScript', age:21},
+{name:'Itzel', job:'Marcy Lab School',language:'JavaScript', age:22},
+{name:'Ray', job:'Square Space',language:'JavaScript', age:21},
+{name:'Jan', job:'Square Space',language:'JavaScript', age:22},
+{name:'Uzma', job:'Thyme Care',language:'JavaScript', age:22}]
 
+
+//1 REDUCE
+
+function averageAge(arr) {
+    return Math.round(arr.reduce((a, b) => {
+            return a + b.age
+    },0) / arr.length)
+}
+console.log(averageAge(alumni))
+
+//2 SORT
+
+function orderedAlumni(arr) {
+    
+}
+
+//3 EVERY
+
+function allUseJavaScript(arr) {
+    
+} 
